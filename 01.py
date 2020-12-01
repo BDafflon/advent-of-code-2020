88ae2d85c5fef0ce98dcb96f4613a7e0c99a439e
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2020-12-01 10:09:34 +0100

    Add day 1 solution

---
+++ 01.py
@@ -0,0 +1,15 @@
+with open("01.in") as f:
+    numbers = [int(x) for x in f.read().split("\n")]
+
+n = len(numbers)
+
+for i, x in enumerate(numbers):
+    for j, y in enumerate(numbers[i + 1 :]):
+        if x + y == 2020:
+            print("Part 1:")
+            print(x * y)
+            print("-" * 10)
+        for z in numbers[i + j + 1 :]:
+            if x + y + z == 2020:
+                print("Part 2:")
+                print(x * y * z)
