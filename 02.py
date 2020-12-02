6aaca0ce1a5d692566ef2a7719cffddfbb8a584f
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2020-12-02 06:50:27 +0100

    Solve day 2

---
+++ 02.py
@@ -0,0 +1,14 @@
+part1 = 0
+part2 = 0
+for line in open("02.in").readlines():
+    arr = line.split()
+    lower, upper = (int(x) for x in arr[0].split("-"))
+    pw = arr[-1]
+    req = arr[1][0]
+    x, y = pw[lower - 1], pw[upper - 1]
+    if pw.count(req) >= lower and pw.count(req) <= upper:
+        part1 += 1
+    if (x == req) ^ (y == req):
+        part2 += 1
+print("Part 1:", part1)
+print("Part 2:", part2)
