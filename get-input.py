efa7d5a43cf63d885b44a5d7de47f80e570147a0
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2020-12-03 09:20:44 +0100

    Add script to download puzzle input

---
+++ get-input.py
@@ -0,0 +1,20 @@
+import sys
+import requests
+
+day = int(sys.argv[1])
+
+with open(".session") as f:
+    session_cookie = f.read()
+
+cookies = {
+    "session": session_cookie,
+}
+
+puzzle_response = requests.get(
+    f"https://adventofcode.com/2020/day/{day}/input", cookies=cookies
+)
+puzzle = puzzle_response.text.rstrip()
+
+filename = "" if day > 9 else "0" + str(day) + ".in"
+with open(filename, "w") as f:
+    f.write(puzzle)
